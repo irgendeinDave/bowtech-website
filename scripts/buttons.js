4c06317b8f10d9161c9dfc2fec579361cf7ed443
irgendeinDave <zollner.david010306@gmail.com>
2023-05-22 18:41:48 +0200

    remove button.js

--- scripts/buttons.js
+++
@@ -1,7 +0,0 @@
-// set the button heights
-
-const buttons = document.querySelectorAll(".button");
-buttons.forEach((button) => {
-  const width = button.offsetWidth;
-  button.style.height = width + "px";
-});
